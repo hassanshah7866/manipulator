6edf8c4107740c1e2f5e4e7feeee9c91d894e6d1
Hassan Shah <hassanshah7866@users.noreply.github.com>
2017-09-11 22:52:21 +0200

    "Created new project and some classes"

---
+++ src/main/java/IntegerList/IntegerIterator.java
@@ -0,0 +1,11 @@
+package IntegerList;
+
+import java.util.ListIterator;
+
+public class IntegerIterator implements ListIterator<Integer> {
+
+    void add(Integer i) {
+
+    }
+
+}
---
+++ src/main/java/Manipulator.java
@@ -0,0 +1,11 @@
+public class Manipulator {
+
+    public static void main (String[] args) {
+
+        IntegerList list = new IntegerList();
+        for (String s : args) {
+            list.add(Integer.getInteger(s);
+        }
+
+    }
+}
